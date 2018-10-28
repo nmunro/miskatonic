d3ff0a45cdd3c9c626f6b55601c6bc176ae7c488
Neil Munro <neilmunro@gmail.com>
2018-10-28 19:30:27 +0000

    Fixed unit tests

--- tests/miskatonic.py
+++ tests/miskatonic.py
@@ -12,11 +12,13 @@ class MainTests(unittest.TestCase):
     def test_create_category(self):
         cat = db.Category.create(title='python')
         self.assertEqual(cat.title, 'python')
+        cat.delete_instance(recursive=True)
 
     def test_create_article(self):
         cat = db.Category.create(title='python')
         article = db.Article.create(title='test', content='meh', category=cat)
         self.assertEqual(article.slug, 'test')
+        cat.delete_instance(recursive=True)
 
 
 if __name__ == '__main__':
